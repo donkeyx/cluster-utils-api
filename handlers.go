d8bb96980cbc71af0d08e90c8896971ab95577a7
David Binney <donkeysoft@gmail.com>
2023-09-02 17:11:32 +0930

    imp #12: logger changes to suger and middlewear
    This adds a middlewear and uses the sugar logger
    which will make logger much nicer and allow flippinig
    from json/text

--- handlers.go
+++ handlers.go
@@ -2,40 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
-
-func authMiddleware(c *gin.Context) {
-	authHeader := c.GetHeader("Authorization")
-
-	if authHeader != "Bearer "+securityToken {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		c.Abort()
-		return
-	}
-
-	c.Next()
-}
-
-func getEnvironmentVariables() map[string]string {
-	envVariables := make(map[string]string)
-	for _, e := range os.Environ() {
-		pair := strings.SplitN(e, "=", 2)
-		if len(pair) == 2 {
-			envVariables[pair[0]] = pair[1]
-		}
-	}
-	return envVariables
-}
-
-func envHandler(c *gin.Context) {
-	envVariables := getEnvironmentVariables()
-	c.JSON(http.StatusOK, envVariables)
-}
 
 func helloHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, World!")
@@ -71,4 +45,61 @@ func headersHandler(c *gin.Context) {
 	}
 
 	c.Data(http.StatusOK, "application/json", headersJSON)
+}
+
+// functions for the routers
+func authMiddleware(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
+
+	if authHeader != "Bearer "+securityToken {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		c.Abort()
+		return
+	}
+
+	c.Next()
+}
+
+func getEnvironmentVariables() map[string]string {
+	envVariables := make(map[string]string)
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		if len(pair) == 2 {
+			envVariables[pair[0]] = pair[1]
+		}
+	}
+	return envVariables
+}
+
+// getClientIP extracts the client's IP address from the request.
+func getClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	if xForwardedFor != "" {
+		return xForwardedFor
+	}
+
+	if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		return ip
+	}
+
+	return ""
+}
+
+func debugHandler(c *gin.Context) {
+	logger, _ := c.Get("logger")
+	sugarLogger, _ := logger.(*zap.SugaredLogger)
+
+	hostname, _ := os.Hostname()
+	sourceIP := getClientIP(c.Request)
+	headers := c.Request.Header
+
+	sugarLogger.Infow("Debug Information",
+		"Hostname", hostname,
+		"SourceIP", sourceIP,
+		"UserAgent", headers.Get("User-Agent"),
+		"Headers", headers,
+		"RequestURI", c.Request.RequestURI,
+	)
+
+	c.JSON(http.StatusOK, gin.H{"message": "Debug information logged"})
 }
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"sync"
-	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -19,17 +19,15 @@ var (
 
 func main() {
 
-	rand.Seed(time.Now().UnixNano())
 	securityToken = generateRandomToken(32)
 
-	// Initialize the logger with JSON output
-	config := zap.NewProductionConfig()
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	logger, _ := config.Build()
-	defer logger.Sync()
+	logger, err := newSugarLogger(false)
+	if err != nil {
+		log.Fatal("Error creating logger:", err)
+	}
 
 	logger.Info("Random Security Token", zap.String("token", securityToken))
-	logCurlCommand(logger)
+	logger.Info("Curl Command", zap.String("command", getCurlCommand(8080, securityToken)))
 
 	isReady = true
 
@@ -46,8 +44,33 @@ func generateRandomToken(length int) string {
 	return string(token)
 }
 
-func logCurlCommand(logger *zap.Logger) {
-	// Log the curl command for the /env endpoint
-	curlCommand := fmt.Sprintf("curl -H 'Authorization: Bearer %s' http://localhost:8080/env", securityToken)
-	logger.Info("Curl Command", zap.String("command", curlCommand))
+func getCurlCommand(port int, securityToken string) string {
+	variable := fmt.Sprintf("curl -H 'Authorization: Bearer %s' http://localhost:8080/env", securityToken)
+	return variable
+}
+
+func newSugarLogger(useJSON bool) (*zap.SugaredLogger, error) {
+	var config zap.Config
+
+	if useJSON {
+		config = zap.NewProductionConfig()
+	} else {
+		config = zap.NewDevelopmentConfig()
+	}
+
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	// Configure the logger to write logs to the terminal
+	config.OutputPaths = []string{"stdout"}
+
+	// Create the logger
+	logger, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a sugar logger from the base logger
+	sugarLogger := logger.Sugar()
+
+	return sugarLogger, nil
 }
