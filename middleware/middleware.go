b9e51b636e8def7ae8c5ab9b91daa74ef7c3641a
David Binney <donkeysoft@gmail.com>
2023-09-18 08:31:02 +0930

    wip #12: Still sorting the zap logger

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,16 +2,29 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func SetupLoggerMiddleware(logger *zap.Logger) gin.HandlerFunc {
+func LoggerMiddleware(logger *zap.Logger) gin.HandlerFunc {
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	// Create a Zap logger with a JSON encoder
+	logger = zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.AddSync(os.Stdout),
+		zap.NewAtomicLevelAt(zap.InfoLevel),
+	), zap.AddCaller())
 
 	return func(c *gin.Context) {
 		// Attach the logger to the context for access in handlers
-		c.Set("logger", logger)
+		c.Set("zapLogger", logger)
 		// Continue with the request
 		c.Next()
 	}
