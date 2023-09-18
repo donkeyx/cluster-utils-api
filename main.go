b070d94f433afef0993d90948364e35f2f83a344
David Binney <donkeysoft@gmail.com>
2023-09-19 07:27:43 +0930

    fix #12: removed the json flipper, not needed atm

--- main.go
+++ main.go
@@ -16,19 +16,13 @@ var securityToken string
 
 func main() {
 
-	useJSON := true
-
-	logger := setupLogger(useJSON)
+	logger := setupLogger()
 	defer logger.Sync()
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()                             // empty engine
 	r.Use(middleware.LoggerMiddleware(logger)) // adds our new middleware
 	r.Use(gin.Recovery())
-
-	// r.Use(middleware.SetupLoggerMiddleware(logger))
-	// r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
-	// r.Use(ginzap.RecoveryWithZap(logger, true))
 
 	securityToken = generateRandomToken(32)
 
@@ -54,32 +48,12 @@ func getCurlCommand(port int, securityToken string) string {
 	return variable
 }
 
-func setupLogger(useJSON bool) *zap.Logger {
-
-	// var logger *zap.Logger
-	// var err error
-	// if useJSON {
-	// 	logger, err = zap.NewProduction()
-	// } else {
-	// 	fmt.Print("Using Development Logger")
-	// 	logger, err = zap.NewDevelopment()
-	// }
-	// if err != nil {
-	// 	panic(err)
-	// }
+func setupLogger() *zap.Logger {
 
 	config := zap.NewProductionConfig()
 	config.Encoding = "json"
 	config.EncoderConfig.TimeKey = "timestamp"
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // Optional: Use ISO8601 time format
-
-	// // config := zap.Config{
-	// // 	Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
-	// // 	Development:      true, // Set this to false in production
-	// // 	EncoderConfig:    encoderConfig,
-	// // 	OutputPaths:      []string{"stdout"},
-	// // 	ErrorOutputPaths: []string{"stderr"},
-	// // }
 
 	logger, err := config.Build()
 	if err != nil {
