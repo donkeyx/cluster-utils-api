ae2d1331a8090d9a15861e9817272fe69ce06dfe
David Binney <david@sine.co>
2024-07-12 13:34:12 +0930

    wip: moved doc to top

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// @title Cluster Util API
+// @version 2.0
+// @description This is a util api which lots of endpoints making it easy to test routing/ingress/egress
+// @host localhost:8080
+// @BasePath /
+
 package main
 
 import (
@@ -16,11 +22,6 @@ import (
 
 var securityToken string
 
-// @title Cluster Util API
-// @version 2.0
-// @description This is a util api which lots of endpoints making it easy to test routing/ingress/egress
-// @host localhost:8080
-// @BasePath /
 func main() {
 
 	logger := setupLogger()
